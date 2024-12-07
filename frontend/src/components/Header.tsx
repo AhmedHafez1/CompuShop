d9edc9becbdf500570b4a553dbf3b74f1e0f1530
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2024-12-07 16:21:05 +0200

    P List Done

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,14 +1,15 @@
-import { Container, Nav, Navbar, NavLink } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 
 import logo from '../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="primary" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand as={NavLink} to="/">
+          <Navbar.Brand as={Link} to="/">
             <img src={logo} alt="CompuShop Logo" />
             CompuShop
           </Navbar.Brand>
@@ -16,11 +17,11 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link as={NavLink} to="/cart">
+              <Nav.Link as={Link} to="/cart">
                 <FaShoppingCart /> Cart
               </Nav.Link>
 
-              <Nav.Link as={NavLink} to="/login">
+              <Nav.Link as={Link} to="/login">
                 <FaUser /> Sign in
               </Nav.Link>
             </Nav>
