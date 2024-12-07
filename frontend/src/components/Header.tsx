aa5d2834aaf094517aae6f9ef8bf3ae7c81b081c
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2024-12-07 13:56:22 +0200

    navlink

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,6 @@
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar, NavLink } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+
 import logo from '../assets/logo.png';
 
 const Header = () => {
@@ -7,17 +8,19 @@ const Header = () => {
     <header>
       <Navbar bg="primary" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">
+          <Navbar.Brand as={NavLink} to="/">
             <img src={logo} alt="CompuShop Logo" />
             CompuShop
           </Navbar.Brand>
+
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart">
+              <Nav.Link as={NavLink} to="/cart">
                 <FaShoppingCart /> Cart
               </Nav.Link>
-              <Nav.Link href="/login">
+
+              <Nav.Link as={NavLink} to="/login">
                 <FaUser /> Sign in
               </Nav.Link>
             </Nav>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App.tsx';
 import './assets/styles/index.css';
 import './assets/styles/bootstrap.custom.css';
+import HomeScreen from './screens/HomeScreen.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomeScreen />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
